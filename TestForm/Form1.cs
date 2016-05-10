6a8965611f1a7c5e9aff1dbb1154cfcf135a5243
刘源 <405653510@qq.com>
2016-05-10 09:12:41 +0800

    提交

--- TestForm/Form1.cs
+++ TestForm/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HID;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,63 @@ namespace TestForm
 {
     public partial class Form1 : Form
     {
+        public Hid hid;
         public Form1()
         {
+            hid = new Hid();
+            hid.vID = 0x0483;
+            hid.pID = 0x0409;
+            hid.DataReceived += Hid_DataReceived;
+            hid.OnSpecifiedDeviceArrived += Hid_OnSpecifiedDeviceArrived;
+            hid.OnSpecifiedDeviceRemoved += Hid_OnSpecifiedDeviceRemoved;
             InitializeComponent();
         }
+        /// <summary>
+        /// HID设备拔出事件触发.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hid_OnSpecifiedDeviceRemoved(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// HID设备插入事件触发.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hid_OnSpecifiedDeviceArrived(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// HID接收数据方法.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Hid_DataReceived(object sender, report e)
+        {
+
+        }
+
+        /// <summary>
+        /// 注册HID设备事件监听.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            hid.RegisterHandle(Handle);
+        }
+        /// <summary>
+        /// HID设备事件拦截.
+        /// </summary>
+        /// <param name="m"></param>
+        protected override void WndProc(ref Message m)
+        {
+            hid.ParseMessages(ref m);
+            base.WndProc(ref m);
+        }
+
     }
 }
